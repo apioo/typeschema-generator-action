0a44da90ee6569ef9f4f0aa04a9a6783fcc5d74e
Christoph Kappestein <christoph.kappestein@gmail.com>
2023-05-28 13:35:50 +0200

    add safe directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@ rm $2/*
 php vendor/psx/schema/bin/schema schema:parse "$1" "$2" --format="$3" --config="$4"
 git config --global user.name "TypeSchema-Bot";
 git config --global user.email "bot@typeschema.org";
+git config --global --add safe.directory /github/workspace
 git add "$2"
 git commit -m "Update code generation"
 git push https://$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git
